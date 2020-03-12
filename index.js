db295f1b322f8c58fb625ecc42ef27860939ab34
ROMUALDO-TXT <lucasrl2003@gmail.com>
2020-03-12 08:34:42 -0300

    Garai

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ console.log("Hello World");
 const NUM_LINHAS = 10;
 
 for(let i = 0; i < NUM_LINHAS; i++){
-    let espaços = 
+    let espaços = ' '.repeat(NUM_LINHAS - i);
+    let conteudo = 'X'.repeat(i);
+    console.log(espaços + conteudo +'X' + '\n');
 }
+console.log((' '.repeat(NUM_LINHAS)+ 'X' + '\n').repeat(4));
