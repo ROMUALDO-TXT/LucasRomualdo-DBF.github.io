f4cd30267fc36382654f0234071a928158bc50d3
ROMUALDO-TXT <61153010+ROMUALDO-TXT@users.noreply.github.com>
2020-03-13 20:58:31 -0300

    PrimeNG

--- index.js
+++ index.js
@@ -3,8 +3,5 @@ console.log("Hello World");
 const NUM_LINHAS = 10;
 
 for(let i = 0; i < NUM_LINHAS; i++){
-    let espaços = ' '.repeat(NUM_LINHAS - i);
-    let conteudo = 'X'.repeat(i);
-    console.log(espaços + conteudo +'X' + '\n');
+    let espaços = 
 }
-console.log((' '.repeat(NUM_LINHAS)+ 'X' + '\n').repeat(4));
